38d2d414f02db0d4ccb1d6b3b51936a732f23df3
Swodesh Singh <romyyoslak@gmail.com>
2026-02-01 19:54:36 +0545

    2nd feb

--- week3PracticalTasks/Main.java
+++ week3PracticalTasks/Main.java
@@ -1,4 +1,44 @@
 package week3PracticalTasks;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+//        Students swodesh = new Students();
+//        swodesh.name = "Swodesh";
+//        swodesh.grade = 'C';
+//        swodesh.update('A');
+//
+//        System.out.println(swodesh.grade);
+
+        OnlineShoppingCart daraz = new OnlineShoppingCart(001,"guitar",20000.5,1);
+        System.out.println(daraz.name);
+
+
+
+    }
+}
+ class Students{
+    int studentID;
+    String name;
+    char grade;
+
+    void update(char Character){
+        grade = Character;
+
+    }
+}
+
+class OnlineShoppingCart{
+    int productid;
+    String name;
+    double price;
+    int stockQuantity;
+
+    OnlineShoppingCart(int productid, String name, double price, int stockQuantity){
+        this.productid = productid;
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
 }
--- week3PracticalTasks/StudentTest.java
+++ week3PracticalTasks/StudentTest.java
@@ -1,10 +1,18 @@
 
 package week3PracticalTasks;
 
+import java.util.Scanner;
+
 class StudentTest {
 
     String newname;
     int newage;
+
+    StudentTest(String name, int age){
+        newname = name;
+        newage = age;
+
+    }
 
 
     void newdisplay() {
@@ -16,9 +24,16 @@ class StudentTest {
 class Testing {
     public static void main(String[] args) {
 
-        StudentTest s1 = new StudentTest();
-        s1.newname = "ram";
-        s1.newage = 20;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the student's name: ");
+        String n = scanner.nextLine();
+
+        System.out.print("Enter the student's age: ");
+        int a = scanner.nextInt();
+
+        StudentTest s1 = new StudentTest(n,a);
         s1.newdisplay();
+
+        scanner.close();
     }
 }
