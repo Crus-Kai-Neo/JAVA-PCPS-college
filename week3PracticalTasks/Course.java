dc57884e5a44fcbd1e26a336d3daeb5628a0f151
Swodesh Singh <romyyoslak@gmail.com>
2026-02-04 18:51:29 +0545

    4th february

---
+++ week3PracticalTasks/Course.java
@@ -0,0 +1,14 @@
+package week3PracticalTasks;
+
+public class Course {
+    String courseID;
+    String courseName;
+    int maxStudents;
+
+    Course(String courseID, String courseName, int maxStudents){
+        this.courseID = courseID;
+        this.courseName = courseName;
+        this.maxStudents = maxStudents;
+    }
+
+}
